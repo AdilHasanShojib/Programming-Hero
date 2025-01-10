167227087f1205cd406fde31fadf9959c63201fb
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2025-01-10 23:08:18 +0600

    Set page layout and Display featured jobs

--- careerHub/src/components/FeaturedJobs/FeaturedJobs.jsx
+++ careerHub/src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -2,30 +2,31 @@ import { useEffect, useState } from "react";
 import Job from "../Job/job";
 
 const FeaturedJobs = () => {
-    const [jobs,setJobs]= useState([]);
+  const [jobs, setJobs] = useState([]);
 
-    useEffect(()=> {
-        fetch('jobs.json').then(res=> res.json()).then(data=>setJobs(data));
-    }, [])
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
 
-    return (
-        <div>
+  return (
+    <div>
+      <div className="text-center">
+        <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
+        <p className="text-center">
+          Explore thousands of job opportunities with all the information you
+          Need. Its Your Future.
+        </p>
+      </div>
 
-<div className="text-center">
-            <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
-            <p className="text-center">Explore thousands of job opportunities with all the information you Need. Its Your Future.</p>
-        </div>
-
-       <div>
-
-       </div>
-            {
-                jobs.map(job=><Job key={job.id} job={job}></Job>)
-            }
-
-        </div>
-        
-    );
+      <div className="grid grid-cols-2 gap-6">
+      {jobs.map((job) => (
+        <Job key={job.id} job={job}></Job>
+      ))}
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedJobs;
