fb5b23ff340e4d7fb00f85ea0fb64f8fbcfca0da
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2025-01-21 12:33:10 +0600

    Basic Setup of React dragonNews

---
+++ dragonNews/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOqrhJ79W4NzfFhSoxqCSw0ihh1s6MYWY",
+  authDomain: "dragonnews-92a13.firebaseapp.com",
+  projectId: "dragonnews-92a13",
+  storageBucket: "dragonnews-92a13.firebasestorage.app",
+  messagingSenderId: "61701905489",
+  appId: "1:61701905489:web:aaf4559db3f72665e9ec70"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
