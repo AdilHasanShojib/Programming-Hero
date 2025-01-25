79cff3a1711e29c6e4c832e83212ed05f0f635d8
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2025-01-25 11:38:15 +0600

    Create Auth Context and integrate Register

--- dragonNews/src/pages/Register/Register.jsx
+++ dragonNews/src/pages/Register/Register.jsx
@@ -1,12 +1,32 @@
 import { Link } from "react-router-dom";
 import Navbar from "../shared/Navbar/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 
 const Register = () => {
+
+ const {createUser} = useContext(AuthContext);
+
+
+
     const handleRegister = e =>{
 
         e.preventDefault();
         const form = new FormData(e.currentTarget);
+
+        const name = form.get('name');
+        const photo = form.get('photo');
+        const email = form.get('email');
+        const password = form.get('password');
+
+        createUser(email,password)
+        .then(res =>{
+          console.log(res);
+        })
+        .catch(err =>{
+          console.log(err);
+        })
 
 
     }
