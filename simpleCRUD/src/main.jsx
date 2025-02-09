3ccf54c037140e0ce7b5699e9fcda5d5254816b3
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2025-02-09 23:13:16 +0600

    Insert single-user Data and find multiple user data

--- simpleCRUD/src/main.jsx
+++ simpleCRUD/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Users from './components/Users.jsx'  ;
 import {
   createBrowserRouter,
   RouterProvider,
@@ -12,6 +13,12 @@ const router = createBrowserRouter([
     path: "/",
     element:<App></App>,
   },
+  {
+    path: "/users",
+    element:<Users></Users>,
+    loader: ()=> fetch('http://localhost:5000/users')
+
+  }
 ]);
 
 createRoot(document.getElementById('root')).render(
